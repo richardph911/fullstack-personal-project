8913b9e445b7d1dc38cb6875d3bf8006d694a125
Dzung Pean <hoangtudauphong@gmail.com>
2021-04-26 23:16:54 -0700

    connected to db

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-
+import mongoose from 'mongoose';
 import posts from '../server/routers/posts.js';
 
 const app = express();
+//use port 5000 localhost
 const PORT = process.env.port || 5000;
+
+//connect to mongodb using uri
+const URI = 'mongodb+srv://admin:dqu0l8Xwwn1GMRn2@cluster0.jfvso.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 //to use middleware,
 app.use(bodyParser.json({limit: '30mb'}));
@@ -19,9 +23,15 @@ app.use( cors());
 app.use('/posts', posts );
 //localhost:5000/
 
-
-
-
-http: app.listen(PORT, () =>{
-    console.log(`Server is running on port ${PORT}`);
+//connect to mongoose db
+mongoose.connect(URI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(()=>{
+        console.log('Connected to DB');
+        app.listen(PORT, () =>{
+            console.log(`Server is running on port ${PORT}`);
+        })
+  })
+    .catch(err =>{
+    console.log('err', err)
 })
+
