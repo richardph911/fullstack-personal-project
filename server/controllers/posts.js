b6a0bb40f0c9e258885e81df245a136005464836
Dzung Pean <hoangtudauphong@gmail.com>
2021-04-26 22:55:33 -0700

    create controllers for app.use

---
+++ server/controllers/posts.js
@@ -0,0 +1,4 @@
+//All method from app.use() will be written here cuz complex and long code
+export const getPosts = (req, res) =>{
+    res.send('ROUTER SUCCESS');
+};
--- server/routers/posts.js
+++ server/routers/posts.js
@@ -1,10 +1,10 @@
 import express from 'express';
 
+import {getPosts} from "../controllers/posts.js";
+
 const router = express.Router();
 
 
-router.get('/', (req, res) =>{
-    res.send( 'ROUTER SUCCESS');
-});
+router.get('/', getPosts);
 
 export default router;
